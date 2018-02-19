11ea46cec71fd0f8451336f34aa668e326b2e99b
JAYARAMGITHUB <35127780+JAYARAMGITHUB@users.noreply.github.com>
2018-02-19 15:59:42 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the integers");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("%d",c);
+if(c%2==0)
+{
+printf("it is even");
+}
+else
+{
+printf("it is odd");
+}return(0);
+}
