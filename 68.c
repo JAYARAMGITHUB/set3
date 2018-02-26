4fbcfa1460d171ca3fcbf2fc88d84bbe46792165
JAYARAMGITHUB <35127780+JAYARAMGITHUB@users.noreply.github.com>
2018-02-26 16:30:22 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter a number");
+scanf("%d",&a);
+if(a%7==0)
+{
+printf("it is a multiple of 7");
+}
+else            
+{
+printf("it is not a multiple of 7");
+}
+return(a);
+}
