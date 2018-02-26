3e0fc977d55bc7d3470e16f7b702e7a63926fe6d
JAYARAMGITHUB <35127780+JAYARAMGITHUB@users.noreply.github.com>
2018-02-26 16:32:30 +0530

    Create 69.c

---
+++ 69.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the integers");
+scanf("%d%d",&a,&b);
+if(a>b)
+{
+c=a-b;
+printf("\n%d",c);
+}
+else
+{
+c=b-a;
+printf("%d",c);
+}
+if(c%2==0)
+{
+printf("\neven");
+}
+else
+{
+printf("\nodd");
+}
+return(0);
+}
