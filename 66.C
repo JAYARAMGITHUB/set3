99777854f14eb5432398b95115848d972d352ded
JAYARAMGITHUB <35127780+JAYARAMGITHUB@users.noreply.github.com>
2018-02-26 15:34:04 +0530

    Create 66.C

---
+++ 66.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n,b=0,i;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+{
+ b++;
+ }        
+}
+if(b==2)
+{
+printf("prime number");
+}
+else
+{
+printf("not a prime number");
+}
+return(0);
+}
