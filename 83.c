6b0f4d2459d8a9c8fb0dfb7f812030b02c736e05
JAYARAMGITHUB <35127780+JAYARAMGITHUB@users.noreply.github.com>
2018-03-16 15:58:35 +0530

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,11 @@
+int main()
+{
+  int a=110,b=100,c=99,d=10,e,f;
+  printf("%d / %d\n",a,b);
+  printf("(%d) % (%d)\n",c,d);
+  e=a/b;
+  f=c%d;
+  printf("%d\n",e);
+  printf("%d\n",f);
+return 0;
+    }
